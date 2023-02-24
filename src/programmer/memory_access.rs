9da7cc540f19aa1b3bf7fce287948ce0060c8a05
Evgeny-SPB <en.kudryavtsev@gmail.com>
2023-02-25 02:02:56 +0300

    memory_write

--- src/programmer/memory_access.rs
+++ src/programmer/memory_access.rs
@@ -54,8 +54,8 @@ impl Programmer
                     return Err(Error::USBDM_Errors(USBDM_ErrorCode::BDM_RC_ILLEGAL_PARAMS))
                 };},
             memory_space_t::MS_WORD => { num_bytes_adjusted /= 2; },
-            memory_space_t::MS_BYTE => { num_bytes_adjusted /= 4; },
-            other               => return Err(Error::USBDM_Errors(USBDM_ErrorCode::BDM_RC_ILLEGAL_PARAMS)),
+            memory_space_t::MS_BYTE => { num_bytes_adjusted /= 1; },
+            other => return Err(Error::USBDM_Errors(USBDM_ErrorCode::BDM_RC_ILLEGAL_PARAMS)),
         };
 
         /*
@@ -111,11 +111,14 @@ impl Programmer
 
         while (bytes_done < num_bytes) {
             let mut block_size: u32 = num_bytes - bytes_done;
+            
             if (block_size > self.bdm_info.dsc_max_memory_read_size.into()) {
                 block_size = self.bdm_info.dsc_max_memory_read_size.into(); }
+            
             let mut data = self.read_memory_block(memory_space, block_size as u8, current_address)?;
             output.append(&mut data);
             bytes_done += block_size;
+            
             if ((memory_space & memory_space_t::MS_SIZE) == memory_space_t::MS_BYTE) {
                 // Byte currentAddress advanced by count of bytes written
                 current_address  += block_size;
@@ -125,5 +128,110 @@ impl Programmer
             }
         }
         Ok(output)
+    }
+
+    //================================================================================
+    // Write X/P memory via ONCE & target execution
+    //
+    // @param memorySpace - Memory space & size of memory accesses 1/2/4 bytes
+    // @param numBytes    - Number of bytes to read (must be a multiple of elementSize)
+    // @param address     - Memory address
+    // @param buffer      - Where to obtain the data
+    //
+    // @note If memory space size is word or long size then address is DSC word address
+    // @note If memory space size is byte size then address is DSC byte pointer address
+    // @note Size is limited to dscInfo.maxMemoryWriteSize
+    //
+    fn write_memory_block(&self, mut memory_space: u8, mut data: Vec<u8>, address: u32) -> Result<(), Error> {
+        if (memory_space == memory_space_t::MS_PLONG) {
+            // Treat as word access
+            memory_space = memory_space_t::MS_PWORD;
+        };
+
+        if data.len() > u8::MAX.into() {return Err(Error::USBDM_Errors(USBDM_ErrorCode::BDM_RC_ILLEGAL_PARAMS))}; 
+        let num_bytes:u8 = data.len() as u8;
+        let mut num_bytes_adjusted = num_bytes;
+        match (memory_space & memory_space_t::MS_SIZE) {
+            memory_space_t::MS_LONG => {
+                if ((address & 0x01) == 0) {
+                    num_bytes_adjusted /= 4;
+                } else {
+                    return Err(Error::USBDM_Errors(USBDM_ErrorCode::BDM_RC_ILLEGAL_PARAMS))
+                };},
+            memory_space_t::MS_WORD => { num_bytes_adjusted /= 2; },
+            memory_space_t::MS_BYTE => { num_bytes_adjusted /= 1; },
+            other => return Err(Error::USBDM_Errors(USBDM_ErrorCode::BDM_RC_ILLEGAL_PARAMS)),
+        };
+
+        /*
+         *    +-----------------------+
+         *    |    JTAG_WRITE_MEM     |
+         *    +-----------------------+
+         *    |    JTAG_END           |
+         *    +-----------------------+
+         *    |                       |
+         *    +--                   --+
+         *    |                       |
+         *    +--  Memory Address   --+
+         *    |                       |
+         *    +--                  ---+
+         *    |                       |
+         *    +-----------------------+
+         *    |  # of memory elements |
+         *    +-----------------------+
+         *    |   Memory Space        |
+         *    +-----------------------+
+         *    | ..................... |
+         *    = ...... data ......... =
+         *    | ..................... |
+         *    +-----------------------+
+         */
+
+        let mut sequence: Vec<u8> = Vec::with_capacity(JTAG_READ_MEMORY_HEADER_SIZE.into());
+        sequence.push(JTAG_WRITE_MEM);          // 0
+        sequence.push(JTAG_END);               // 1
+        sequence.push((address >> 24) as u8);  // 2 Address
+        sequence.push((address >> 16) as u8);  // 3
+        sequence.push((address >> 8) as u8);   // 4
+        sequence.push(address as u8);          // 5
+        sequence.push(num_bytes_adjusted);     // 6 Elements
+        sequence.push(memory_space);           // 7 Memory space
+        sequence.append(&mut data);
+  
+        self.exec_jtag_seq(sequence, 0)?;
+        Ok(())
+    }
+
+    //================================================================================
+    // Write X/P memory via ONCE & target execution
+    //
+    // @param memorySpace - Memory space & size of memory accesses 1/2/4 bytes
+    // @param numBytes    - Number of bytes to read (must be a multiple of elementSize)
+    // @param address     - Memory address
+    // @param buffer      - Where to obtain the data
+    //
+    // @note If memory space size is word or long size then address is DSC word address
+    // @note If memory space size is byte size then address is DSC byte pointer address
+    //
+    pub fn dsc_write_memory(&self, mut memory_space: u8, mut data: Vec<u8>, mut address: u32) -> Result<(), Error> {
+
+        while (data.len() > 0) {
+            let mut block_size = data.len();
+            
+            if block_size > self.bdm_info.dsc_max_memory_write_size.into() {
+                block_size = self.bdm_info.dsc_max_memory_write_size.into(); };
+            
+            self.write_memory_block(memory_space, data.split_off(block_size), address)?;
+            
+            if ((memory_space & memory_space_t::MS_SIZE) == memory_space_t::MS_BYTE) {
+                // Byte currentAddress advanced by count of bytes written
+                address  += block_size as u32;
+            } else {
+                // Address advanced by count of words written
+                address  += (block_size / 2) as u32;
+            }
+        }
+     
+        Ok(())
     }
 }
