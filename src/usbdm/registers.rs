bf2bda49fee435d2c70bb01826574c6d83d49c3a
Evgeny-SPB <en.kudryavtsev@gmail.com>
2023-03-09 23:31:51 +0300

    dsc_registers_add

--- src/usbdm/registers.rs
+++ src/usbdm/registers.rs
@@ -6,6 +6,71 @@ const DSC_FIRST_ONCE_REGISTER: u8 = 39;
 const DSC_LAST_ONCE_REGISTER: u8 = 56;
 const DSC_ONCE_REGISTER_COUNT: u8 = DSC_LAST_ONCE_REGISTER - DSC_FIRST_ONCE_REGISTER + 1;
 
+// regNo Parameter for DSC_ReadReg() with DSC target
+// DSC Core registers
+enum DscRegisters {
+    // Core registers
+    DscRegX0      =  0,  // 0
+    DscRegY0      =  1,
+    DscRegY1      =  2,
+    DscRegA0      =  3,
+    DscRegA1      =  4,
+    DscRegA2      =  5,
+    DscRegB0      =  6,
+    DscRegB1      =  7,
+    DscRegB2      =  8,
+    DscRegC0      =  9,
+    DscRegC1      = 10,  // 10
+    DscRegC2      = 11,
+    DscRegD0      = 12,
+    DscRegD1      = 13,
+    DscRegD2      = 14,
+    DscRegOmr     = 15,
+    DscRegSr      = 16,
+    DscRegLa      = 17,
+    DscRegLa2     = 18,  // read only
+    DscRegLc      = 19,
+    DscRegLc2     = 20,  // 20 read only
+    DscRegHws0    = 21,
+    DscRegHws1    = 22,
+    DscRegSp      = 23,
+    DscRegN3      = 24,
+    DscRegM01     = 25,
+    DscRegN       = 26,
+    DscRegR0      = 27,
+    DscRegR1      = 28,
+    DscRegR2      = 29,
+    DscRegR3      = 30,  // 30
+    DscRegR4      = 31,
+    DscRegR5      = 32,
+    DscRegsHm01   = 33,
+    DscRegsHn     = 34,
+    DscRegsHr0    = 35,
+    DscRegsHr1    = 36,
+    DscRegPc      = 37,
+    // JTAG registers
+    DscRegIdcode  = 38,  // JTAG Core IDCODE
+    // ONCE registers
+    DscRegOcr     = 39,  // 39 ONCE Control register
+    DscRegOscntr  = 40,  // ONCE Instruction Step Counter
+    DscRegOsr     = 41,  // ONCE Status register
+    DscRegOpdbr   = 42,  // ONCE Program Data Bus Register
+    DscRegObase   = 43,  // ONCE Peripheral Base Address regitsre
+    DscRegOtxrxsr = 44,  // ONCE Tx & Rx Status & Control register
+    DscRegOtx     = 45,  // ONCE Transmit register (32-bit)
+    DscRegOtx1    = 46,  // ONCE Transmit register (16-bit)
+    DscRegOrx     = 47,  // ONCE Receive register (32-bit)
+    DscRegOrx1    = 48,  // ONCE Receive register (16-bit)
+    DscRegOtbcr   = 49,  // ONCE Trace buffer control register
+    DscRegOtbpr   = 50,  // ONCE Trace Buffer Pointer register
+    DscRegOtb     = 51,  // Trace Buffer Register Stages
+    DscRegOb0cr   = 52,  // Breakpoint Unit 0 Control register
+    DscRegOb0ar1  = 53,  // Breakpoint Unit 0 Address register 1
+    DscRegOb0ar2  = 54,  // Breakpoint Unit 0 Address register 2
+    DscRegOb0msk  = 55,  // Breakpoint Unit 0 Mask register
+    DscRegOb0Cntr = 56,  // Breakpoint Unit 0 Counter
+ }
+
 pub struct TargetRegisterDetails {
     name: &'static str,
     width: u8,
